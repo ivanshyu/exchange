309fc27c082d3f300b02acb336a20c618f7ac750
Nowvan <xul4m3z06@gmail.com>
2020-01-06 14:36:20 +0800

    加一堆頁面

--- routes/index.js
+++ routes/index.js
@@ -18,6 +18,57 @@ router.get('/register', function(req, res, next) {
   res.render('register');
 });
 
+router.get('/myfavourite', function(req, res, next) {
+  res.render('myfavourite');
+});
+
+router.get('/myaccount', function(req, res, next) {
+  res.render('myaccount');
+});
+
+router.get('/orderinformation', function(req, res, next) {
+  res.render('orderinformation');
+});
+
+router.get('/newitems', function(req, res, next) {
+  res.render('newitems');
+});
+
+router.get('/managementitems', function(req, res, next) {
+  res.render('managementitems');
+});
+
+router.get('/wishlist', function(req, res, next) {
+  res.render('wishlist');
+});
+
+router.get('/newwishlist', function(req, res, next) {
+  res.render('newwishlist');
+});
+
+router.get('/announcement', function(req, res, next) {
+  res.render('announcement');
+});
+
+router.get('/guide', function(req, res, next) {
+  res.render('guide');
+});
+
+router.get('/commonproblem', function(req, res, next) {
+  res.render('commonproblem');
+});
+
+router.get('/chatroom', function(req, res, next) {
+  res.render('chatroom');
+});
+
+router.get('/advancedsearch', function(req, res, next) {
+  res.render('advancedsearch');
+});
+
+
+advancedsearch
+
 
 
 
