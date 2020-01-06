d1d10b18e09b343d530dd485adca3d1df84af0d0
Nowvan <xul4m3z06@gmail.com>
2020-01-06 17:44:49 +0800

    footer new item

---
+++ public/pagejs/newitem.js
@@ -0,0 +1,27 @@
+
+$("#progressbarTWInput").change(function(){
+
+    readURL(this);
+
+});
+
+
+function readURL(input){
+
+    if(input.files && input.files[0]){
+
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+
+            $("#preview_progressbarTW_img").attr('src', e.target.result);
+
+        }
+
+        reader.readAsDataURL(input.files[0]);
+
+    }
+
+}
+
+
